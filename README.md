f45f3cf12a5627afb16659238c10c99c2f65cb95
Kazuo Matsunaga <kazuomatz@lifecode.jp>
2021-05-05 15:47:20 +0900

    first release

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# README
+# Magick Pen
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Magick Penは、Railsプロジェクトに組み込んで利用できるGemのプラグイン [ Magick Pen ]( https://github.com/kazuomatz/magick_pen ) のサンプルのRailsプロジェクトです。
+プラグインの機能は、 [ こちら ]( https://github.com/kazuomatz/magick_pen ) を参考にして下さい。
 
-Things you may want to cover:
+## Installation
 
-* Ruby version
+```bash
+$ bundle
+$ rails g magick_pen:install --with-migrate
+$ rails s 
+```
 
-* System dependencies
+ブラウザーから http://localhost:3000 にアクセスするとデモページが開きます。
 
-* Configuration
+## 動作環境
 
-* Database creation
+このプロジェクトは、Ruby 2.7 / Rails 6.1 で作ってありますが、Ruby 2.6 〜 3.0 / Rails 6.0 ・ 6.1 で動作します。
 
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MagickPenTest
+module MagickPenDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.precompile += %w( application.js application.css bulma/application.css bulma/application.js bootstrap/application.css bootstrap/application.js)
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+
+    I18n.available_locales = %i(ja en)
+    I18n.enforce_available_locales = true
+    I18n.default_locale = :ja
+
   end
 end
